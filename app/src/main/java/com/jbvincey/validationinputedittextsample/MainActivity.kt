10e084dd4fd1d6342d63ffdd45d05c8a8b89aac1
jbvincey <jbvincey@deezer.com>
2018-10-27 11:44:51 +0200

    add menu on MainActivity

--- app/src/main/java/com/jbvincey/validationinputedittextsample/MainActivity.kt
+++ app/src/main/java/com/jbvincey/validationinputedittextsample/MainActivity.kt
@@ -3,6 +3,8 @@ package com.jbvincey.validationinputedittextsample
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.Snackbar
+import android.view.Menu
+import android.view.MenuItem
 import com.jbvincey.validationinputedittext.ValidationInputEditTextListener
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -14,6 +16,25 @@ class MainActivity : AppCompatActivity() {
 
         validationInputEditText.validationInputEditTextListener = ValidationInputEditTextListener {
             Snackbar.make(activity_main_root, R.string.text_validated, Snackbar.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_main -> {
+                validationInputEditText.validateText()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
