3d42a2a4cf5db3f655f85268098e34acf22d4542
jbvincey <jbvincey@deezer.com>
2018-10-27 10:48:58 +0200

    add ValidationInputEditText

---
+++ validationinputedittext/src/main/java/com/jbvincey/validationinputedittext/ValidationInputEditTextListener.java
@@ -0,0 +1,12 @@
+package com.jbvincey.validationinputedittext;
+
+import android.support.annotation.Nullable;
+
+/**
+ * Created by jbvincey on 26/10/2018.
+ */
+public interface ValidationInputEditTextListener {
+
+    void onTextValidated(@Nullable String text);
+
+}
