2894e805ea0b1ef4d9138057fc2b959b49c68a33
jbvincey <jbvincey@deezer.com>
2018-10-27 11:38:42 +0200

    implement MainActivity with ValidationInputEditText

--- app/src/main/java/com/jbvincey/validationinputedittextsample/MainActivity.kt
+++ app/src/main/java/com/jbvincey/validationinputedittextsample/MainActivity.kt
@@ -2,11 +2,18 @@ package com.jbvincey.validationinputedittextsample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import com.jbvincey.validationinputedittext.ValidationInputEditTextListener
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        validationInputEditText.validationInputEditTextListener = ValidationInputEditTextListener {
+            Snackbar.make(activity_main_root, R.string.text_validated, Snackbar.LENGTH_LONG).show()
+        }
     }
 }
